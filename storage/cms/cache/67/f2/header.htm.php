fcee91ecbc54cf8df639fc1f1a91bd4165d956fb
TaiLA <luuanhtai92@gmail.com>
2021-03-16 12:01:45 +0700

    git ignore

--- storage/cms/cache/67/f2/header.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms605017df3ea0a478698615_e06697be6ff3e378c79227cd820480a9Class extends Cms\Classes\PartialCode
-{
-
-}
--- storage/cms/cache/98/56/cart.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms604740a4cd26f975676893_f5f311cb02790f680af9c8a0225bcb2cClass extends Cms\Classes\PageCode
-{
-
-}
